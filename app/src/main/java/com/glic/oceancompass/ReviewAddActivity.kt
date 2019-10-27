cc8b61fc3fd514d906c9f6eb930095492095697a
Buleok <rr2424@naver.com>
2019-10-27 21:19:02 +0900

    글쓰기 페이지

--- app/src/main/java/com/glic/oceancompass/ReviewAddActivity.kt
+++ app/src/main/java/com/glic/oceancompass/ReviewAddActivity.kt
@@ -3,13 +3,16 @@ package com.glic.oceancompass
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.main.*
+import kotlinx.android.synthetic.main.reviewadd.*
 
 class ReviewAddActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reviewadd)
-
+        upload.setOnClickListener {
+        }
     }
 
     override fun onBackPressed() {
