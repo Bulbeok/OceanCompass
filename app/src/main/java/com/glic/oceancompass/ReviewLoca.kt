dacdadb47f6e7f5d90240c2b74d79be11bb60224
Buleok <rr2424@naver.com>
2019-10-28 18:51:43 +0900

    서치 전체 클릭

---
+++ app/src/main/java/com/glic/oceancompass/ReviewLoca.kt
@@ -0,0 +1,12 @@
+package com.glic.oceancompass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReviewLoca : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_loca)
+    }
+}
