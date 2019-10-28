56777c6e0275adb57a0c5f9b63f05106792956ae
N4using <dnjsxo516@naver.com>
2019-10-28 22:58:36 +0900

    웹뷰와 자바스크립트 연동

--- app/src/main/java/com/glic/oceancompass/ReviewLocaActivity.kt
+++ app/src/main/java/com/glic/oceancompass/ReviewLocaActivity.kt
@@ -1,13 +1,22 @@
 package com.glic.oceancompass
 
+import android.content.Context
+import android.content.Intent
 import android.net.http.SslError
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.webkit.*
 import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.review.*
 
 class ReviewLocaActivity : AppCompatActivity() {
+
+    companion object {
+        var context : Context? = null
+        var handler : Handler? = Handler()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +24,15 @@ class ReviewLocaActivity : AppCompatActivity() {
 
         NukeSSLCerts().nuke()
 
+        context = this
+
         search.setOnClickListener {
             search.isIconified = false
         }
 
         search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
-                map.loadUrl("https://175.206.239.109:8443/oceancompass/mobilemap.jsp?search=$query")
+                map.loadUrl("https://175.206.239.109:8443/oceancompass/mobilemap2.jsp?address=$query")
                 return true
             }
             override fun onQueryTextChange(newText: String): Boolean {
@@ -31,6 +42,7 @@ class ReviewLocaActivity : AppCompatActivity() {
 
         map.settings.javaScriptEnabled = true
         map.settings.domStorageEnabled = true
+        map.addJavascriptInterface(WebBrideg(),"address")
         map.loadUrl("https://175.206.239.109:8443/oceancompass/mobilemap2.jsp")
         map.webViewClient = object : WebViewClient(){
             override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
@@ -42,5 +54,20 @@ class ReviewLocaActivity : AppCompatActivity() {
                 callback.invoke(origin, true, false)
             }
         }
+    }
+
+    class WebBrideg{
+        @JavascriptInterface
+        fun getAddress(num : String){
+            handler?.post {
+                Toast.makeText(context,"선택한 위치는 $num 입니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        startActivity(Intent(this,ReviewAddActivity::class.java))
+        finish()
+        overridePendingTransition(0, 0)
     }
 }
