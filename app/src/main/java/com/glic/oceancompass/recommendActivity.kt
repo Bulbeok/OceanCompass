9cbc81fddcd7e14e220515de51df527c35b457c4
Buleok <rr2424@naver.com>
2019-10-29 22:01:34 +0900

    메인메인

---
+++ app/src/main/java/com/glic/oceancompass/recommendActivity.kt
@@ -0,0 +1,12 @@
+package com.glic.oceancompass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recommendActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommend)
+    }
+}
