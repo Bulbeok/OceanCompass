06e6f8f54383563eeed025b20c0158d1a04ccf9c
N4using <dnjsxo516@naver.com>
2019-10-29 23:21:43 +0900

    종료시 온습도 유지

--- app/src/main/java/com/glic/oceancompass/MainActivity.kt
+++ app/src/main/java/com/glic/oceancompass/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.glic.oceancompass
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -19,6 +20,9 @@ open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+
+        val pref = this.getSharedPreferences("sessionCookie", Context.MODE_PRIVATE)
+        val edit = pref.edit()
 
         NukeSSLCerts().nuke()
 
@@ -63,6 +67,48 @@ open class MainActivity : AppCompatActivity() {
             tutlehinttext.visibility = View.INVISIBLE
         }
 
+        if(pref.getString("location", "") != "") {
+            location.visibility = View.VISIBLE
+            tutlehint.visibility = View.INVISIBLE
+            tutlehinttext.visibility = View.INVISIBLE
+            location.setText(pref.getString("location", ""))
+            val request = object : StringRequest(
+                Method.POST, "https://175.206.239.109:8443/oceancompass/WeatherServlet",
+                //요청 성공 시
+                Response.Listener {
+                    if(it.trim() == "1") {
+                        Toast.makeText(this,"위치를 다시 입력해 주세요",Toast.LENGTH_LONG).show()
+                    } else {
+                        edit.putString("location", location.text.toString())
+                        edit.apply()
+                        when(Integer.parseInt(it.split(",")[3])) {
+                            1, 4 -> backgroundweather.setBackgroundResource(R.drawable.background_rain)
+                            2, 3 -> backgroundweather.setBackgroundResource(R.drawable.background_snow)
+                            else -> backgroundweather.setBackgroundResource(R.drawable.background1)
+                        }
+                        temperature.text = it.split(",")[0]
+                        humidity.text = it.split(",")[1]
+                    }
+                },
+                // 에러 발생 시
+                Response.ErrorListener {
+                    Log.e("에러", "[${it.message}]")
+                }) {
+                override fun getParams(): Map<String, String> {
+                    val params = HashMap<String, String>()
+                    params["loca"] = pref.getString("location", "")!!
+                    return params
+                }
+            }
+            val queue = Volley.newRequestQueue(this)
+            queue.add(request)
+        } else {
+            location.visibility = View.INVISIBLE
+            tutlehint.visibility = View.VISIBLE
+            tutlehinttext.visibility = View.VISIBLE
+            location.setText("")
+        }
+
         location.setOnKeyListener { _, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
                 if(location.text.toString() != "") {
@@ -73,6 +119,8 @@ open class MainActivity : AppCompatActivity() {
                             if(it.trim() == "1") {
                                 Toast.makeText(this,"위치를 다시 입력해 주세요",Toast.LENGTH_LONG).show()
                             } else {
+                                edit.putString("location", location.text.toString())
+                                edit.apply()
                                 when(Integer.parseInt(it.split(",")[3])) {
                                     1, 4 -> backgroundweather.setBackgroundResource(R.drawable.background_rain)
                                     2, 3 -> backgroundweather.setBackgroundResource(R.drawable.background_snow)
@@ -94,6 +142,9 @@ open class MainActivity : AppCompatActivity() {
                     }
                     val queue = Volley.newRequestQueue(this)
                     queue.add(request)
+                } else {
+                    edit.putString("location", "")
+                    edit.apply()
                 }
             }
             false
