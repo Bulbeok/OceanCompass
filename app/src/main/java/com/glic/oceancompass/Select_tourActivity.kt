287af3774967e6af049ea0841a764def0232439d
Buleok <rr2424@naver.com>
2019-10-31 18:40:20 +0900

    정말 한게 없네

---
+++ app/src/main/java/com/glic/oceancompass/Select_tourActivity.kt
@@ -0,0 +1,12 @@
+package com.glic.oceancompass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Select_tourActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_tour)
+    }
+}
