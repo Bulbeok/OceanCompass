e0b8635454349d87acbc462756ee4d2cf6bca1e9
Buleok <rr2424@naver.com>
2019-10-31 16:17:39 +0900

    오늘은 뭔가한거같은데 뭔지 모르겠네

--- app/src/main/java/com/glic/oceancompass/recommendActivity.kt
+++ app/src/main/java/com/glic/oceancompass/recommendActivity.kt
@@ -2,11 +2,14 @@ package com.glic.oceancompass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_recommend.*
 
 class recommendActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recommend)
+        complete.setOnClickListener {
+            }
+        }
     }
-}
