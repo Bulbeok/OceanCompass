0982794ab77dfe168c065a9ac5f0e45c881847fc
N4using <dnjsxo516@naver.com>
2019-10-31 22:16:59 +0900

    4차 업로드 / 뒤로가기 기능 추가

--- app/src/main/java/com/glic/oceancompass/RecommendActivity.kt
+++ app/src/main/java/com/glic/oceancompass/RecommendActivity.kt
@@ -99,4 +99,8 @@ class RecommendActivity : AppCompatActivity() {
         }
     }
 
+    override fun onBackPressed() {
+        startActivity(Intent(this,SearchActivity::class.java))
+        finish()
+    }
 }
